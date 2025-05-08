9fe0105195c393aa160cca60a8d96e775d4e8a61
Michael Khanzadeh <matteenk@gmail.com>
2025-05-08 23:16:31 +0000

    Human eval metrics for quantized models

---
+++ quantization/run_human_eval.sh
@@ -0,0 +1,7 @@
+python quantization/run_human_eval.py \
+  --model_id meta-llama/Llama-3.1-8B \
+  --quant 16 \
+  --num_tasks 164 \
+  --num_samples 1 \
+  --wandb_project final_project \
+  --wandb_entity ns3888-hpml
